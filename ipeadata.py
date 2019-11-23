b1f8744c85892d72e73c052ece835b932531563d
phelipe <phelipe_teles@hotmail.com>
2019-11-23 14:02:20 -0300

    working get_series and get for ipeadata

--- ipeadata.py
+++ ipeadata.py
@@ -1,8 +1,21 @@
-import requests
-from helpers.json import clean_json
+from pandas import concat
+from helpers.types import parse_cods
+from helpers.request import get_serie
+from helpers.response import parse_response
 
 
-def get(cod, start=None, end=None):
+def get(cod, nome=None, start=None, end=None, out="pd"):
     base_url = "http://ipeadata2-homologa.ipea.gov.br/api/v1/ValoresSerie(SERCODIGO='"
     url = base_url + cod + "')"
-    return clean_json(requests.get(url).text)
+    return parse_response(get_serie(url), cod, nome, out, source="ipea")
+
+
+def get_series(*codigos, start=None, end=None, last_n=None, join="outer", **kwargs):
+    codigos, nomes = parse_cods(*codigos)
+    return concat(
+        [get(cod, start, end) for cod in codigos],
+        axis="columns",
+        join=join,
+        sort=True,  # done to avoid pandas warning messages
+        **kwargs
+    ).rename(columns={cod: nome for nome, cod in zip(nomes, codigos)})
