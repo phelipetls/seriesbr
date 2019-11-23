1c5e5ee840e5dcd6cc1c58f395f6b2fc48b982c2
phelipe <phelipe_teles@hotmail.com>
2019-11-23 14:24:24 -0300

    parse response of requests in this module

---
+++ helpers/response.py
@@ -0,0 +1,40 @@
+from pandas import read_json
+from json import loads
+from pandas import DataFrame
+from datetime import date
+
+
+def parse_response(json, cod, nome, out, source):
+    if out == "raw":
+        return json
+    elif out == "pd":
+        if source == "bcb":
+            return parse_bcb_json(json, cod, nome)
+        elif source == "ipea":
+            return parse_ipea_json(json, cod, nome)
+        else:
+            raise ValueError("Invalid source.")
+    else:
+        return "{out.capitalize()} is not a valid output format."
+
+
+def parse_bcb_json(raw_json, cod, nome):
+    try:
+        json_df = read_json(raw_json, dtype={"data": "datetime64", "valor": "float"})
+    except ValueError:
+        print(f"Request for {cod} produces invalid json.")
+        raise
+    return json_df.set_index("data").rename(columns={"valor": nome if nome else cod})
+
+
+def parse_ipea_json(raw_json, cod, nome):
+    json_values = loads(raw_json)["value"]
+    if json_values == []:
+        raise ValueError(f"Request for {cod} returns no values.")
+    return DataFrame([
+        {
+            "date": date(item["ANO"], item["MES"], item["DIA"]).strftime("%d/%m/%Y"),
+            "value": item["VALVALOR"],
+        }
+        for item in json_values
+    ]).set_index("date").rename(columns={"value": nome if nome else cod})
