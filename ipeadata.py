f909eb21126b3773f2150e77180971ebdb95a0ad
phelipe <phelipe_teles@hotmail.com>
2019-11-23 19:44:00 -0300

    little fix because of get request wrapper

--- ipeadata.py
+++ ipeadata.py
@@ -7,7 +7,7 @@ from helpers.response import parse_response
 def get(cod, nome=None, start=None, end=None, out="pd"):
     base_url = "http://ipeadata2-homologa.ipea.gov.br/api/v1/ValoresSerie(SERCODIGO='"
     url = base_url + cod + "')"
-    return parse_response(get_serie(url), cod, nome, out, source="ipea")
+    return parse_response(custom_get(url).text, cod, nome, out, source="ipea")
 
 
 def get_series(*codigos, start=None, end=None, last_n=None, join="outer", **kwargs):
