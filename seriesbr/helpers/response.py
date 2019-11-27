6fe14913fbebee5b63bfacc17fb80a687005d69c
phelipe <phelipe_teles@hotmail.com>
2019-11-27 16:41:57 -0300

    renaming from "nome" to "name"

--- seriesbr/helpers/response.py
+++ seriesbr/helpers/response.py
@@ -2,29 +2,29 @@ import pandas as pd
 from datetime import datetime
 
 
-def parse_response(response, code, nome, source):
+def parse_response(response, code, name, source):
     if source == "bcb":
-        return parse_bcb_json(response, code, nome)
+        return parse_bcb_json(response, code, name)
     elif source == "ipea":
-        return parse_ipea_json(response, code, nome)
+        return parse_ipea_json(response, code, name)
     else:
         print("Invalid source.")
         return
 
 
-def parse_bcb_json(response, code, nome):
+def parse_bcb_json(response, code, name):
     json = response.json()
     assert json, print(f"Request for {code} returned nothing.")
-    return json_to_dataframe(json, code, nome, "data", "valor", "%d/%m/%Y")
+    return json_to_dataframe(json, code, name, "data", "valor", "%d/%m/%Y")
 
 
-def parse_ipea_json(response, code, nome):
+def parse_ipea_json(response, code, name):
     json = response.json()["value"]
     assert json, print(f"Request for {code} returned nothing.")
-    return json_to_dataframe(json, code, nome, "VALDATA", "VALVALOR", "%Y-%m-%dT%H:%M:%S%z")
+    return json_to_dataframe(json, code, name, "VALDATA", "VALVALOR", "%Y-%m-%dT%H:%M:%S%z")
 
 
-def json_to_dataframe(json, code, nome, date_key, value_key, date_fmt):
+def json_to_dataframe(json, code, name, date_key, value_key, date_fmt):
     D = {"date": [], "value": []}
     date_value_generator = ((item[date_key], item[value_key]) for item in json)
     for date, value in date_value_generator:
@@ -33,5 +33,5 @@ def json_to_dataframe(json, code, nome, date_key, value_key, date_fmt):
     dataframe = pd.DataFrame(D)
     dataframe["date"] = pd.to_datetime(dataframe["date"], format="%d/%m/%Y")
     dataframe["value"] = dataframe["value"].astype("float64")
-    dataframe = dataframe.set_index("date").rename(columns={"value": nome if nome else code})
+    dataframe = dataframe.set_index("date").rename(columns={"value": name if name else code})
     return dataframe
