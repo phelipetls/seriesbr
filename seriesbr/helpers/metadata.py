d4e8794006c9cccbf077339e16f905ec00a4419c
phelipe <phelipe_teles@hotmail.com>
2019-11-27 08:44:28 -0300

    do not rename the variables to be queried in ipea search

--- seriesbr/helpers/metadata.py
+++ seriesbr/helpers/metadata.py
@@ -1,27 +1,27 @@
 # Dictionaries of metadatas to query
 
-ipea_metadata_list = {
-    "name": "SERNOME",
-    "base": "BASNOME",
-    "theme": "TEMNOME",
-    "unit": "UNINOME",
-    "period": "PERNOME",
-    "source": "FNTNOME",
-    "code": "SERCODIGO",
-    "source_code": "FNTSIGLA",
-    "region_code": "PAICODIGO",
-    "description": "SERCOMENTARIO",
-}
+ipea_metadata_list = [
+    "SERNOME",
+    "BASNOME",
+    "TEMNOME",
+    "UNINOME",
+    "PERNOME",
+    "FNTNOME",
+    "SERCODIGO",
+    "FNTSIGLA",
+    "PAICODIGO",
+    "SERCOMENTARIO",
+]
 
 
-def ipea_make_select_query(selection):
+def ipea_make_select_query(queried_fields):
     ordem = ["SERCODIGO", "PERNOME", "UNINOME", "SERNOME"]
-    defaults = set({"SERCODIGO", "PERNOME", "UNINOME", "SERNOME"})
+    default = set({"SERCODIGO", "PERNOME", "UNINOME", "SERNOME"})
     # to get the string
     # SERCODIGO,PERNOME,UNINOME,SERNOME,ANOTHERFILTER,ANOTHERFILTER
     # where ANOTHER must be something not alreay selected in default
-    additional = set([ipea_metadata_list[selected] for selected in selection]) - defaults
-    queried = ordem + additional
+    not_in_default = set(field for field in queried_fields) - default
+    queried = ordem + list(not_in_default)
     return f"?$select={','.join(queried)}"
 
 
@@ -31,7 +31,7 @@ def ipea_make_filter_query(name, filters):
     filter_query = f"&$filter=contains(SERNOME,'{name}')"
     if filters:
         filter_arguments = "and contains" + " and contains".join(
-            f"({ipea_metadata_list[metadata]},'{value}')"
+            f"({metadata},'{value}')"
             for metadata, value in filters.items()
         )
     return f"{filter_query}{filter_arguments if filters else ''}"
