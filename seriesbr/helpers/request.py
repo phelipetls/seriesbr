d8691c81a85626413bcc9cee1089fc426cfd4bed
phelipe <phelipe_teles@hotmail.com>
2019-11-28 09:41:39 -0300

    requests within session

--- seriesbr/helpers/request.py
+++ seriesbr/helpers/request.py
@@ -1,7 +1,8 @@
 import requests
 
+s = requests.Session()
 
 def custom_get(url, **kwargs):
-    response = requests.get(url, timeout=120, **kwargs)
+    response = s.get(url, timeout=120, **kwargs)
     response.raise_for_status()
     return response
