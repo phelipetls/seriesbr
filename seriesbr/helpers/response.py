ca2096a8a7d005ace8aa1d9a0953bc9770263501
phelipe <phelipe_teles@hotmail.com>
2019-11-27 22:56:34 -0300

    just some change in formatting

--- seriesbr/helpers/response.py
+++ seriesbr/helpers/response.py
@@ -18,7 +18,9 @@ def parse_bcb_json(response, code, name):
 def parse_ipea_json(response, code, name):
     json = response.json()["value"]
     assert json, print(f"Request for {code} returned nothing.")
-    return json_to_dataframe(json, code, name, "VALDATA", "VALVALOR", "%Y-%m-%dT%H:%M:%S%z")
+    return json_to_dataframe(
+        json, code, name, "VALDATA", "VALVALOR", "%Y-%m-%dT%H:%M:%S%z"
+    )
 
 
 def json_to_dataframe(json, code, name, date_key, value_key, date_fmt):
@@ -30,5 +32,7 @@ def json_to_dataframe(json, code, name, date_key, value_key, date_fmt):
     dataframe = pd.DataFrame(D)
     dataframe["date"] = pd.to_datetime(dataframe["date"], format="%d/%m/%Y")
     dataframe["value"] = dataframe["value"].astype("float64")
-    dataframe = dataframe.set_index("date").rename(columns={"value": name if name else code})
+    dataframe = dataframe.set_index("date").rename(
+        columns={"value": name if name else code}
+    )
     return dataframe
