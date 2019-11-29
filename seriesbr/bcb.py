227865d0fc4c9e03d77e94164a8092b8110bcd54
phelipe <phelipe_teles@hotmail.com>
2019-11-29 16:40:35 -0300

    stop pandas deprecation warnings

--- seriesbr/bcb.py
+++ seriesbr/bcb.py
@@ -69,6 +69,7 @@ def get_series(*codes, start=None, end=None, last_n=None, **kwargs):
         (get_serie(code, name, start, end, last_n)
          for code, name in zip(codes, names)),
         axis="columns",
+        sort=True,
         **kwargs
     )
 
