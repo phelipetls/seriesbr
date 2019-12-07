e283a017d0b1dcdf7d99738a42a4eeefece5a620
phelipe <phelipe_teles@hotmail.com>
2019-12-06 23:35:43 -0300

    ibge: search mechanism in list_functions etc.

--- seriesbr/ibge.py
+++ seriesbr/ibge.py
@@ -23,6 +23,7 @@ def get_series(
     macroregion=None,
     microregion=None,
     mesoregion=None,
+    brazil=None,
     classifications=None,
 ):
     """
@@ -108,12 +109,12 @@ def list_aggregates(*search, where="nome"):
 
     Returns
     -------
-    A DataFrame with information regarding the aggregates.
+    A DataFrame with metadata about the aggregates.
     """
     json = requests.get("https://servicodados.ibge.gov.br/api/v3/agregados").json()
     df = pd.io.json.json_normalize(json, record_path="agregados")
     if search:
-        return df.query(f'{where}.str.contains(@search)', engine='python')
+        return do_search(df, search, where)
     return df
 
 
@@ -140,36 +141,77 @@ def list_variables(aggregate_code):
     return pd.io.json.json_normalize(json).iloc[:, :3]
 
 
-def list_classifications(aggregate_code, search=None, where="nome"):
+def list_periods(aggregate_code):
+    """
+    Function to list periods of a given aggregate.
+
+    Returns
+    -------
+    A DataFrame with the frequency, initial and
+    final dates.
+    """
+    baseurl = "https://servicodados.ibge.gov.br/api/v3"
+    query = f"/agregados/{aggregate_code}/metadados"
+    url = f"{baseurl}{query}"
+    json = get_json(url)["periodicidade"]
+    df = pd.DataFrame.from_dict(json, orient="index", columns=["value"])
+    df.index = ["frequency", "start", "end"]
+    return df
+
+
+def list_classifications(aggregate_code, *search, where="nome"):
+    """
+    Function to list all classification of a given aggregate.
+
+    Parameters
+    ----------
+    search : str
+        String to search.
+
+    where : str
+        Where to search.
+
+    Returns
+    -------
+    A DataFrame with the classifications and their categories.
+    """
     baseurl = "https://servicodados.ibge.gov.br/api/v3/agregados"
     url = f"{baseurl}/{aggregate_code}/metadados"
-    json = custom_get(url).json()
+    json = get_json(url)
     df1 = pd.io.json.json_normalize(json, ['classificacoes', 'categorias'], meta=['classificacoes'])
     df2 = df1.classificacoes.apply(pd.Series).iloc[:, :2]
     df2 = df2.rename(lambda x: "classificacao_" + x, axis='columns')
     df = pd.concat([df1.drop('classificacoes', axis='columns'), df2], axis='columns')
     if search:
-        return df.query(f'{where}.str.contains(@search)', engine='python')
+        return do_search(df, search, where)
     return df
 
 
-def list_states(search=None, where="nome"):
+def list_states(*search, where="nome"):
     """
     Function to list all states and their codes.
 
+    Parameters
+    ----------
+    search : str
+        String to search.
+
+    where : str
+        Where to search.
+
     Returns
     -------
-    A DataFrame with information about the states.
+    A DataFrame with metadata about the states.
     """
     url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/"
     json = get_json(url)
     df = clean_json(json)
     if search:
-        return df.query(f'{where}.str.contains(@search)', engine='python')
+        return do_search(df, search, where)
     return df
 
 
-def list_macroregions(search=None, where="nome"):
+def list_macroregions(*search, where="nome"):
     """
     Function to list all macroregions and their codes.
 
@@ -189,39 +231,85 @@ def list_macroregions(search=None, where="nome"):
     json = get_json(url)
     df = clean_json(json)
     if search:
-        return df.query(f'{where}.str.contains(@search)', engine='python')
+        return do_search(df, search, where)
     return df
 
 
-def list_cities(search=None, where="nome"):
+def list_cities(*search, where="nome"):
+    """
+    Function to lsit all cities and their codes.
+
+    Parameters
+    ----------
+    search : str
+        String to search.
+
+    where : str
+        Where to search.
+
+    Returns
+    -------
+    A DataFrame with metadata about the cities.
+    """
     url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios"
     json = get_json(url)
     df = clean_json(json)
     if search:
-        return df.query(f'{where}.str.contains(@search)', engine='python')
+        return do_search(df, search, where)
     return df
 
 
-def list_microregions(search=None, where="nome"):
+def list_microregions(*search, where="nome"):
+    """
+    Function to lsit all microregions.
+
+    Parameters
+    ----------
+    search : str
+        String to search.
+
+    where : str
+        Where to search.
+
+    Returns
+    -------
+    A DataFrame with metadata about the microregions.
+    """
     url = "https://servicodados.ibge.gov.br/api/v1/localidades/microrregioes"
     json = get_json(url)
     df = clean_json(json)
     if search:
-        return df.query(f'{where}.str.contains(@search)', engine='python')
+        return do_search(df, search, where)
     return df
 
 
-def list_mesoregions(search=None, where="nome"):
+def list_mesoregions(*search, where="nome"):
     url = "https://servicodados.ibge.gov.br/api/v1/localidades/mesorregioes"
     json = get_json(url)
     df = clean_json(json)
     if search:
-        return df.query(f'{where}.str.contains(@search)', engine='python')
+        return do_search(df, search, where)
     return df
+
+## Helpers
 
 
 def clean_json(json):
+    """
+    Helper function to transform JSON into
+    a DataFrame and clean its columns names.
+    """
     df = pd.io.json.json_normalize(json, sep='_')
     df = df.rename(lambda x: x.replace('.', '_'), axis='columns')
     df = df.rename(lambda x: '_'.join(re.split(r'_', x)[-2:]), axis='columns')
     return df
+
+
+def do_search(df, search, where, prefix=""):
+    """
+    Helper function to search for regex
+    in a given column
+    """
+    # (?iu) sets unicode and ignore case flags
+    to_search = r'(?iu)' + '|'.join(search)
+    return df.query(f"{where}.str.contains(@to_search)", engine='python')
