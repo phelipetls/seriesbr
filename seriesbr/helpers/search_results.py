29bd1e156b178f949f48a369ef859916395b6652
phelipe <phelipe_teles@hotmail.com>
2019-12-06 23:32:57 -0300

    ipea/bcb: simplifying conversion into df of search results

--- seriesbr/helpers/search_results.py
+++ seriesbr/helpers/search_results.py
@@ -1,25 +1,14 @@
 from pandas import DataFrame
 
 
-def return_search_results_bcb(response):
-    json = response.json()
+def return_search_results_bcb(json):
     search_results = json["result"]["results"]
     assert search_results, "Nothing was found."
     metadata_list = ["codigo_sgs", "title", "periodicidade", "unidade_medida"]
-    D = {metadata: [] for metadata in metadata_list}
-    for result in search_results:
-        for metadata in metadata_list:
-            D[metadata].append(result[metadata])
-    df = DataFrame(D)
-    return df
+    return DataFrame(search_results).loc[:, metadata_list]
 
 
-def return_search_results_ipea(response):
-    search_results = response.json()["value"]
+def return_search_results_ipea(json):
+    search_results = json["value"]
     assert search_results, "Nothing was found."
-    D = {metadata: [] for metadata in search_results[0].keys()}
-    for result in search_results:
-        for metadata in result.keys():
-            D[metadata].append(result[metadata])
-    df = DataFrame(D)
-    return df
+    return DataFrame(search_results)
