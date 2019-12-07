d740bccacf806b35d3925bd5abd1ae84c330986f
phelipe <phelipe_teles@hotmail.com>
2019-12-06 23:32:21 -0300

    ibge: parse input dates as well

--- seriesbr/helpers/dates.py
+++ seriesbr/helpers/dates.py
@@ -1,13 +1,14 @@
-from datetime import datetime, timezone, timedelta
+import datetime
 
-year_format = ["%Y", "%y"]
-month_year_format = ["%m/%y", "%m/%Y", "%M/%Y", "%b/%Y", "%B/%y"]
+year_format = ["%Y"]
+month_year_format = ["%m/%y", "%m/%Y", "%M/%Y"]
 day_month_year_format = ["%d/%m/%Y"]
 
 slash_formats = day_month_year_format + year_format + month_year_format
 dash_formats = [fmt.replace("/", "-") for fmt in slash_formats]
+blank_formats = [fmt.replace("/", "") for fmt in slash_formats]
 
-allowed_fmts = slash_formats + dash_formats
+allowed_fmts = slash_formats + dash_formats + blank_formats
 
 
 def parse_date(date, api, start=True):
@@ -15,13 +16,13 @@ def parse_date(date, api, start=True):
     for fmt in allowed_fmts:
         try:
             if start:
-                date_object = datetime.strptime(date, fmt)
+                date_object = datetime.datetime.strptime(date, fmt)
             else:
                 # if an end date,
                 # "2011"    -> "31-12-2011"
                 # "01-2011" -> "31-01-2011"
                 # "02-01-2011" -> "02-01-2011"
-                date = datetime.strptime(date, fmt)
+                date = datetime.datetime.strptime(date, fmt)
                 date_object = date.replace(
                     month=12 if fmt in year_format else date.month,
                     day=last_day_of_month(date) if fmt.find("%d") == -1 else date.day,
@@ -29,7 +30,7 @@ def parse_date(date, api, start=True):
             return api_date_format_of(date_object, api)  # handle different api date formats
         except ValueError:
             continue
-    raise ValueError
+    raise ValueError("Not a valid date format.")
 
 
 def parse_dates(start, end, api):
@@ -37,8 +38,14 @@ def parse_dates(start, end, api):
     Auxiliary function to convert different date formats
     to %d/%m/%Y, which is required by the SGS API.
     """
-    start = parse_date(start, api, start=True) if start else ""
-    end = parse_date(end, api, start=False) if end else ""
+    if start:
+        start = parse_date(start, api, start=True)
+    else:
+        start = api_date_format_of(very_old_date(), api)
+    if end:
+        end = parse_date(end, api, start=False)
+    else:
+        end = api_date_format_of(today_date(), api)
     return start, end
 
 
@@ -48,10 +55,8 @@ def api_date_format_of(date, api):
         return date.strftime("%Y-%m-%dT00:00:00") + utc_offset  # 01-12-2010T00:00:00-03:00
     if api == "bcb":
         return date.strftime("%d/%m/%Y")  # 01/12/2010
-
-
-def get_utc_offset():
-    return datetime.now(timezone.utc).astimezone().isoformat()[-6:]
+    if api == "ibge":
+        return date.strftime("%Y%m")  # 201901
 
 
 def last_day_of_month(date):
@@ -61,5 +66,17 @@ def last_day_of_month(date):
 
     See: https://stackoverflow.com/questions/42950/get-last-day-of-the-month
     """
-    next_month = date.replace(day=28) + timedelta(days=4)
-    return (next_month - timedelta(days=next_month.day)).day
+    next_month = date.replace(day=28) + datetime.timedelta(days=4)
+    return (next_month - datetime.timedelta(days=next_month.day)).day
+
+
+def get_utc_offset():
+    return datetime.datetime.now(datetime.timezone.utc).astimezone().isoformat()[-6:]
+
+
+def very_old_date():
+    return datetime.datetime(1900, 1, 1)
+
+
+def today_date():
+    return datetime.datetime.today()
