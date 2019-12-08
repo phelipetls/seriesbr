0fdcf8e93a257d84eb30720e97bb60b507e4963e
phelipe <phelipe_teles@hotmail.com>
2019-12-07 21:45:09 -0300

    ready to publish v0.0.5

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import distutils
 long_description = """
 SeriesBR - A Python package to get brazilian economic time series
 
-It has functions to get time series data from the Instituto de Pesquisa Econômica Aplicada (IPEA) and Brazilian Central Bank (BCB) databases into a pandas.DataFrame.
+It has functions to get time series data from the Instituto de Pesquisa Econômica Aplicada (IPEA), Brazilian Central Bank (BCB) and Instituto Brasileiro de Geografia e Estatística (IBGE) databases into a pandas.DataFrame.
 
 There are also functions to make queries and to get metadata information.
 
-You can discover more about it in its GitHub [repository](https://github.com/phelipetls/seriesbr).
+You can find more about at the GitHub [repository](https://github.com/phelipetls/seriesbr).
 """
 
 setuptools.setup(
     name="seriesbr",
-    version="0.0.4",
+    version="0.0.5",
     author="Phelipe Teles",
     author_email="phelipe_teles@hotmail.com",
     description="Get requests for brazilian economic time series databases",
