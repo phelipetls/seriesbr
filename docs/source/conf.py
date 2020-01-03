8351468db058b5bcde534b5a3beb110e2b085bf1
phelipe <phelipe_teles@hotmail.com>
2020-01-03 00:02:24 -0300

    ready to publish v0.1.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Phelipe Teles'
 author = 'Phelipe Teles'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1.2'
+release = 'v0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
