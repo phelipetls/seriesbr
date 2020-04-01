8e3a95c4889eab8aa05e73dab7c882a0440fce4b
phelipe <phelipe_teles@hotmail.com>
2020-04-01 07:56:43 -0300

    test dataframes dtypes after parsing json

--- tests/test_json_parsers.py
+++ tests/test_json_parsers.py
@@ -21,19 +21,32 @@ def mocked_get_json(json):
 
 @patch("seriesbr.helpers.response.get_json", mocked_get_json)
 class TestJsonParsers(unittest.TestCase):
-    """Test getting a timeseries in JSON format and parsing it into a dataframe"""
+    """Test getting a timeseries in JSON format and parsing it into a DataFrame"""
 
     def test_bcb_parser(self):
         df = bcb_json_to_df(get_sample_json("bcb_json.json"), 11, "Selic")
+        # assert it is a DataFrame
         self.assertIsInstance(df, pd.DataFrame)
+        # index is datetime
+        self.assertTrue(pd.api.types.is_datetime64_dtype(df.index))
+        # series is float
+        self.assertTrue(pd.api.types.is_float_dtype(df.values))
 
     def test_ipea_parser(self):
         df = ipea_json_to_df(get_sample_json("ipea_json.json"), "Código", "Nome")
+        # assert it is a DataFrame
         self.assertIsInstance(df, pd.DataFrame)
+        # index is datetime
+        self.assertTrue(pd.api.types.is_datetime64_dtype(df.index))
+        # series is float
+        self.assertTrue(pd.api.types.is_float_dtype(df.values))
 
     def test_ibge_parser(self):
         df = ibge_json_to_df(get_sample_json("ibge_json.json"))
+        # assert it is a DataFrame
         self.assertIsInstance(df, pd.DataFrame)
+        # index is datetime
+        self.assertTrue(pd.api.types.is_datetime64_dtype(df.index))
 
 
 if __name__ == "__main__":
