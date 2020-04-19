2c6c6f462f5a5d69bb7357e17bb1db9f3eab58c8
phelipe <phelipe_teles@hotmail.com>
2020-04-19 12:41:11 -0300

    version 0.1.8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Phelipe Teles'
 author = 'Phelipe Teles'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1.7'
+release = 'v0.1.8'
 
 
 # -- General configuration ---------------------------------------------------
