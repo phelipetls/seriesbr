f5dec6850a08ee49d2539ab08c9fcbd76320eafe
Phelipe Teles <phelipe_teles@hotmail.com>
2022-01-05 22:22:42 -0300

    Use consistent series code in tests

--- tests/test_bcb.py
+++ tests/test_bcb.py
@@ -140,16 +140,16 @@ def test_bcb_get_series_dataframe():
 def test_bcb_get_metadata():
     responses.add(
         responses.GET,
-        "https://dadosabertos.bcb.gov.br/api/3/action/package_search?fq=codigo_sgs:20786",
+        "https://dadosabertos.bcb.gov.br/api/3/action/package_search?fq=codigo_sgs:11",
         json={
             "result": {
-                "results": [{"code": "20786"}],
+                "results": [{"code": "11"}],
             },
         },
         status=200,
     )
 
-    df = bcb.get_metadata(20786)
-    expected_df = pd.DataFrame({"values": ["20786"]}, index=pd.Series(["code"]))
+    df = bcb.get_metadata(11)
+    expected_df = pd.DataFrame({"values": ["11"]}, index=pd.Series(["code"]))
 
     pd.testing.assert_frame_equal(df, expected_df)
