ccd9affc8959f0bb80c4230851d63aa1215e0d30
Phelipe Teles <phelipe_teles@hotmail.com>
2022-01-08 20:38:32 -0300

    Refactor how to transform IBGE JSON payload into dataframe

--- seriesbr/ibge/series.py
+++ seriesbr/ibge/series.py
@@ -107,54 +107,56 @@ def format_date(date, frequency):
     return date.strftime(get_date_format(frequency))
 
 
+ibge_columns = {
+    "territory_code": "NC",
+    "territory_name": "NN",
+    "unit_of_measurement_code": "MC",
+    "unit_of_measurement_name": "MN",
+    "value": "V",
+    "location_code": "D1C",
+    "location_name": "D1N",
+    "period_code": "D2C",
+    "period_name": "D2N",
+    "variable_name": "D3C",
+    "variable_code": "D3N",
+    "classification_code": "D4C",
+    "classification_name": "D4N",
+}
+
+selected_ibge_columns = [
+    ibge_columns["value"],
+    ibge_columns["location_code"],
+    ibge_columns["period_code"],
+    ibge_columns["variable_name"],
+    ibge_columns["variable_code"],
+    ibge_columns["classification_name"],
+]
+
+
 def build_df(json, freq="mensal"):
-    # first element contains only metadata
-    # let's ignore it
-    df = pd.DataFrame(json[1:])
+    columns, data = json[0], json[1:]
 
-    # getting columns names
-    df.columns = json[0].values()
+    date_column = columns["D2C"]
 
-    date_key = json[0]["D2C"]
-    date_fmt = get_date_format(freq)
+    df = pd.DataFrame(data)
+    df = df.loc[:, selected_ibge_columns]
 
-    # handling dates
+    df.columns = [
+        label for code, label in columns.items() if code in selected_ibge_columns
+    ]
+
     if freq == "trimestral":
-        df[date_key] = pd.PeriodIndex(
-            df[date_key].str.replace(r"([0-9]{4}).([0-9])", r"\1-Q\2", regex=True),
+        df[date_column] = pd.PeriodIndex(
+            df[date_column].str.replace(r"([0-9]{4}).([0-9])", r"\1-Q\2", regex=True),
             freq="Q",
         ).to_timestamp()
     else:
-        df[date_key] = pd.to_datetime(df[date_key], format=date_fmt)
+        df[date_column] = pd.to_datetime(df[date_column], format=get_date_format(freq))
 
-    df = df.set_index(date_key)
+    df = df.set_index(date_column)
     df = df.rename_axis("Date")
 
-    # handling numerical values
     df["Valor"] = pd.to_numeric(df["Valor"], errors="coerce")
-
-    # getting the specific location column name
-    location_key = json[0]["D1C"]
-
-    # drop these columns
-    to_drop = ["Mês", "Unidade de Medida", "Brasil", "Nível Territorial"]
-
-    # also every columns that is a code
-    is_code = lambda column: column.endswith("(Código)")  # noqa: E731
-
-    # except for the location and variable code
-    is_location_code = lambda column: column == location_key  # noqa: E731
-    is_variable_code = lambda column: column == "Variável (Código)"  # noqa: E731
-
-    to_drop.extend(
-        [
-            c
-            for c in df.columns
-            if is_code(c) and not (is_location_code(c) or is_variable_code(c))
-        ]
-    )
-
-    df = df.drop(to_drop, axis="columns", errors="ignore")
 
     return df
 
