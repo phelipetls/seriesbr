f6d4811ae762fb4ad6c011ab99a93f2fcef877d6
Phelipe Teles <phelipe_teles@hotmail.com>
2022-01-08 15:54:52 -0300

    Remove bizarre KeyError handling
    This should never happen

--- seriesbr/ipea/series.py
+++ seriesbr/ipea/series.py
@@ -48,17 +48,13 @@ def build_df(json, code, label):
     json = json["value"]
     df = pd.DataFrame(json)
 
-    # removing utc component from date string
-    try:
-        df["VALDATA"] = df["VALDATA"].str[:-6]
-        df = df.set_index("VALDATA")
-        df = df.rename_axis("Date")
-        df.index = pd.to_datetime(df.index, format="%Y-%m-%dT%H:%M:%S")
-        # casting numerical values
-        df["VALVALOR"] = pd.to_numeric(df["VALVALOR"], errors="coerce")
-        df.columns = [label or code]
-    except KeyError:
-        return
+    df["VALDATA"] = df["VALDATA"].str[:-6]
+    df = df.set_index("VALDATA")
+    df = df.rename_axis("Date")
+    df.index = pd.to_datetime(df.index, format="%Y-%m-%dT%H:%M:%S")
+
+    df["VALVALOR"] = pd.to_numeric(df["VALVALOR"], errors="coerce")
+    df.columns = [label or code]
 
     return df
 
